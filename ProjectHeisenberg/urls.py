05a45e4bf0f1eaeffae2cf23314f76d34cc7af6a
Luuk Holleman <luukholleman@gmail.com>
2014-11-12 14:41:02 +0100

    Added static file serving for debugging

--- ProjectHeisenberg/urls.py
+++ ProjectHeisenberg/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,4 +10,4 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     (r'^', include('base.urls'))
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
