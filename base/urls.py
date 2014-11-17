763c0bce20940b474c5952c79a4054387a27e038
Luuk Holleman <luukholleman@gmail.com>
2014-11-17 14:29:40 +0100

    Fixed bug in last commit

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
-from django.contrib.auth.models import User
 from base.views import HomepageTemplateView
+from rest_framework import routers
+
 router = routers.DefaultRouter()
 
 urlpatterns = patterns('',
