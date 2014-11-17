d895d74a3e3db4a7185d3deb0df3b68e7a76e435
Luuk Holleman <luukholleman@gmail.com>
2014-11-17 14:07:57 +0100

    Installed django rest framework

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.models import User
 from base.views import HomepageTemplateView
+router = routers.DefaultRouter()
 
 urlpatterns = patterns('',
     url(r'^$', HomepageTemplateView.as_view(), name='home'),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
