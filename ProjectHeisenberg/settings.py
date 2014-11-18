57f38242e76132a41f7d51606786e86fac76bc89
Luuk Holleman <luukholleman@gmail.com>
2014-11-18 15:45:19 +0100

    Enabled token bases logins

--- ProjectHeisenberg/settings.py
+++ ProjectHeisenberg/settings.py
@@ -59,8 +59,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissions'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     )
 }
 
