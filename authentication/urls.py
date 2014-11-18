75c2f0d68bff57cf5eed2d689986de3b36887d6b
Luuk Holleman <luukholleman@gmail.com>
2014-11-18 13:18:07 +0100

    Implemented custom user class

---
+++ authentication/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import include, url
+from rest_framework import routers
+from authentication.views import UserViewSet
+
+router = routers.DefaultRouter()
+
+router.register(r'users', UserViewSet)
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+]
