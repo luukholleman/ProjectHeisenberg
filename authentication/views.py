c449c97b55ed8c722e6811d9151ab780d9e577a3
Luuk Holleman <luukholleman@gmail.com>
2014-11-18 15:11:07 +0100

    Separated AuthenticatedOrAnonReadAndCreate in different classes

--- authentication/views.py
+++ authentication/views.py
@@ -2,11 +2,11 @@ from rest_framework import viewsets
 from rest_framework.decorators import permission_classes
 
 from authentication.models import User
-from base.rest.permissions import AuthenticatedOrAnonReadAndCreate
+from base.rest.permissions import AuthenticatedOrAnonReadAndCreate, IsSelf
 from authentication.serializers import UserSerializer
 
 
-@permission_classes((AuthenticatedOrAnonReadAndCreate,))
+@permission_classes((AuthenticatedOrAnonReadAndCreate,IsSelf))
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
