51eeb972242efdd8650b6b32d9aec3e6c18e3014
Leon Huzen <leonhuzen@gmail.com>
2014-11-18 10:13:10 +0100

    partials to templates

--- base/static/app/app.js
+++ base/static/app/app.js
@@ -5,11 +5,11 @@ var app = angular.module('punktlich', [
     function ($routeProvider) {
         $routeProvider.
             when('/login', {
-                templateUrl: 'partials/login.html',
+                templateUrl: 'templates/login.html',
                 controller: 'authenticationController'
             }.
             when('/register', {
-                templateUrl: 'partials/register.html',
+                templateUrl: 'templates/register.html',
                 controller: 'authenticationController'
             }).
             otherwise({
