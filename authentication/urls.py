0d0dc585eb87249f3c7b8287639c6b007e34fc7c
Daan Houweling <daan.gdh@gmail.com>
2014-11-19 14:04:08 +0100

    Disabled trailing slash in rest framework

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from rest_framework import routers
 from authentication.views import UserViewSet
 
-router = routers.DefaultRouter()
+router = routers.DefaultRouter(trailing_slash=False)
 
 router.register(r'users', UserViewSet)
 
