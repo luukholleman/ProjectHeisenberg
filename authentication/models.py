e5b2983db4f33fa8445ea45618432da1c135a829
Luuk Holleman <luukholleman@gmail.com>
2014-11-19 12:05:58 +0100

    Login by emailaddress

--- authentication/models.py
+++ authentication/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import AbstractUser
-
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 class User(AbstractUser):
-    pass
+    email_address = models.EmailField(_('email address'), blank=True, unique=True)
+
+    USERNAME_FIELD = 'email_address'
