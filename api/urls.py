380aa303665cbff94cdac4d7d69fc26cc5acb8fd
Luuk Holleman <luukholleman@gmail.com>
2014-11-20 13:43:27 +0100

    Fixed the trailing slash in api root url

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import include, url
 from api.views import APIRoot
 
 urlpatterns = [
-    url(r'overview', APIRoot.as_view(), name='api.root'),
     url('', include('api.authentication.urls')),
 ]
--- api/views.py
+++ api/views.py
@@ -6,9 +6,11 @@ from rest_framework.reverse import reverse
 class APIRoot(APIView):
     permission_classes = ()
 
-    def get(self, request):
+    def get(self, request, *args):
         return Response({
             'login': reverse('api.v1.login', request=request),
             'get-authenticated-user': reverse('api.v1.get-authenticated-user', request=request),
             'users': reverse('api.v1.user-list', request=request),
         })
+
+api_root = APIRoot.as_view()
