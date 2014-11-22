90cca04b57ada41a4054d7f2e0fa49f9db5f939a
Tim Potze <timpotze@gmail.com>
2014-11-22 14:58:42 +0100

    Added working timeline element

--- base/static/app/controllers/timelineController.js
+++ base/static/app/controllers/timelineController.js
@@ -1,3 +1,7 @@
 angular.module('punktlichDep').controller('TimelineController', function ($scope) {
     //stub
+
+    $scope.generateDate = function(year,month,day) {
+        return new Date(year, month -1, day, 0, 0, 0).getTime()/1000;
+    }
 });
