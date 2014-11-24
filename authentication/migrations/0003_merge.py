185b6ba21b2f70ec7fc482c57fb4b9653370ce7a
Luuk Holleman <luukholleman@gmail.com>
2014-11-24 10:07:17 +0100

    Merged migrations

---
+++ authentication/migrations/0003_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('authentication', '0002_seed'),
+        ('authentication', '0002_auto_20141120_1112'),
+    ]
+
+    operations = [
+    ]
