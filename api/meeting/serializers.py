0f57db296c104199f3804e652db5962d00e917ee
Daan Houweling <daan.gdh@gmail.com>
2014-11-25 17:03:54 +0100

    Set required fields

--- api/meeting/serializers.py
+++ api/meeting/serializers.py
@@ -4,9 +4,9 @@ from meeting.models import Meeting
 
 class MeetingSerializer(serializers.Serializer):
     name = serializers.CharField(required=True, max_length=90)
-    description = serializers.CharField()
-    location = serializers.CharField()
-    address = serializers.CharField()
+    description = serializers.CharField(required=False)
+    location = serializers.CharField(required=False)
+    address = serializers.CharField(required=False)
     date_and_time = serializers.DateTimeField(required=True)
     participants = serializers.RelatedField()
 
