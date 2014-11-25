6d41ff012800414af4b37408f73eac877944d8c9
Daan Houweling <daan.gdh@gmail.com>
2014-11-25 19:11:09 +0100

    Added tests

--- api/meeting/views.py
+++ api/meeting/views.py
@@ -1,4 +1,4 @@
-from django.utils.dateparse import parse_date
+from django.utils.dateparse import parse_datetime
 from rest_framework import generics, viewsets
 from api.meeting.serializers import MeetingSerializer
 from meeting.models import Meeting
@@ -6,11 +6,11 @@ from meeting.models import Meeting
 
 class MeetingViewSet(viewsets.ModelViewSet):
     serializer_class = MeetingSerializer
+    #todo add logged in permission here
     permission_classes = []
 
     def get_queryset(self):
-
-        from_date = parse_date(self.request.QUERY_PARAMS['from'])
-        to_date = parse_date(self.request.QUERY_PARAMS['to'])
+        from_date = parse_datetime(self.request.QUERY_PARAMS['from'])
+        to_date = parse_datetime(self.request.QUERY_PARAMS['to'])
 
         return Meeting.objects.filter(date_and_time__range=[from_date, to_date])
