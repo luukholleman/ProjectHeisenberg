7cd6f0fbcea1bb4774610b6d2d145abf0f0d4e4f
Daan Houweling <daan.gdh@gmail.com>
2014-11-25 17:21:59 +0100

    Added filtering

--- api/meeting/views.py
+++ api/meeting/views.py
@@ -1,9 +1,16 @@
+from django.utils.dateparse import parse_date
 from rest_framework import generics, viewsets
 from api.meeting.serializers import MeetingSerializer
 from meeting.models import Meeting
 
 
 class MeetingViewSet(viewsets.ModelViewSet):
-    queryset = Meeting.objects.all()
     serializer_class = MeetingSerializer
     permission_classes = []
+
+    def get_queryset(self):
+
+        from_date = parse_date(self.request.QUERY_PARAMS['from'])
+        to_date = parse_date(self.request.QUERY_PARAMS['to'])
+
+        return Meeting.objects.filter(date_and_time__range=[from_date, to_date])
