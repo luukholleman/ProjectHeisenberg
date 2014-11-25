1eab7abbab32cd8eca34558f8fab6761651a536c
Tim Potze <timpotze@gmail.com>
2014-11-25 10:05:18 +0100

    added yearly,monthly,dayly signs to timeline

--- base/static/app/controllers/timelineController.js
+++ base/static/app/controllers/timelineController.js
@@ -1,7 +1,7 @@
 angular.module('punktlichDep').controller('TimelineController', function ($scope) {
     //stub
 
-    $scope.generateDate = function(year,month,day) {
-        return new Date(year, month -1, day, 0, 0, 0).getTime()/1000;
+    $scope.generateDate = function(year,month,day, h, m, s) {
+        return new Date(year, month -1, day, h,m,s).getTime()/1000;
     }
 });
