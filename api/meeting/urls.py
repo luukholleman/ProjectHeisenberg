b03cb794de6cc3aee852125aa8cb83960adb12f5
Daan Houweling <daan.gdh@gmail.com>
2014-11-25 16:58:51 +0100

    Fixed routing

--- api/meeting/urls.py
+++ api/meeting/urls.py
@@ -1,9 +1,8 @@
-from django.conf.urls import include, url
 from rest_framework import routers
 from api.meeting.views import MeetingViewSet
 
 router = routers.DefaultRouter(trailing_slash=False)
 
-router.register(r'', MeetingViewSet, base_name='api.v1.meeting')
+router.register(r'meetings', MeetingViewSet, base_name='api.v1.meeting')
 
 urlpatterns = router.urls
--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import include, url
-from api.views import APIRoot
+from rest_framework.routers import DefaultRouter
+from api.authentication.views import UserViewSet
+from api.meeting.views import MeetingViewSet
+
+router = DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
-    url(r'', include('api.authentication.urls')),
-    url(r'meetings', include('api.meeting.urls'))
+    url(r'^', include(router.urls)),
+    url(r'^', include('api.meeting.urls')),
+    url(r'^', include('api.authentication.urls'))
 ]
