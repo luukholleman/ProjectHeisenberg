688470a074d7a46850787d85eb2c0fcedb88f2a0
Leon Huzen <leonhuzen@gmail.com>
2014-11-26 11:33:10 +0100

    add meeting to timeline

--- base/static/app/controllers/timelineController.js
+++ base/static/app/controllers/timelineController.js
@@ -1,7 +1,24 @@
 angular.module('punktlichDep').controller('TimelineController', function ($scope) {
-    //stub
+    $scope.groups = [];
+    $scope.meetings = [];
 
-    $scope.generateDate = function(year,month,day, h, m, s) {
-        return new Date(year, month -1, day, h,m,s).getTime()/1000;
-    }
+    $scope.addMeeting = function (color, date) {
+        $scope.meetings.push({
+            color: color,
+            date: date,
+            visible: true
+        })
+    };
+
+    $scope.toggleGroupVisible = function (color) {
+        $scope.meetings.filter(function (e) {
+            return e.color == color;
+        }).forEach(function (e) {
+            e.visible = false;
+        });
+    };
+
+    $scope.generateDate = function (year, month, day, h, m, s) {
+        return new Date(year, month - 1, day, h, m, s).getTime() / 1000;
+    };
 });
