2c7ee60a58177324f0fcb0f53966284d7b435da4
Leon Huzen <leonhuzen@gmail.com>
2014-11-26 13:22:42 +0100

    punktlich meeting

--- base/static/app/controllers/timelineController.js
+++ base/static/app/controllers/timelineController.js
@@ -1,12 +1,25 @@
 angular.module('punktlichDep').controller('TimelineController', function ($scope, $timeout) {
     $scope.meetings = [];
 
+    var init = function(){
+        $scope.addMeeting('punktlich', $scope.generateDate(2014, 12, 1, 10, 0, 0));
+        $scope.addMeeting('green', $scope.generateDate(2014, 11, 27, 0, 0, 0));
+        $scope.addMeeting('pink', $scope.generateDate(2014, 11, 26, 10, 5, 0));
+        $scope.addMeeting('yellow', $scope.generateDate(2014, 12, 3, 16, 0, 0));
+        $scope.addMeeting('red', $scope.generateDate(2014, 12, 5, 20, 0, 0));
+        $timeout(function () {
+            document.getElementById('timeline').refresh();
+        })
+    }
+
     $scope.addMeeting = function (color, date) {
         $scope.meetings.push({
+//            id: id,
             color: color,
             date: date,
             visible: true
         });
+        //  Timeout takes care of a callback ofter $apply, this is needed because we want the added meeting to show.
         $timeout(function () {
             document.getElementById('timeline').refresh();
         })
@@ -23,4 +36,6 @@ angular.module('punktlichDep').controller('TimelineController', function ($scope
     $scope.generateDate = function (year, month, day, h, m, s) {
         return new Date(year, month - 1, day, h, m, s).getTime() / 1000;
     };
+
+    init();
 });
