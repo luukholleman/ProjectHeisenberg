574e69aa1b19f49ff2246c5c3637fbc7fb76450f
Daan Houweling <daan.gdh@gmail.com>
2014-12-07 16:31:14 +0100

    Removed spinner from loading bar

--- base/static/app/app.js
+++ base/static/app/app.js
@@ -3,10 +3,12 @@ var apiBase = '/api/v1/';
 
 angular.module('punktlich', ['punktlichDep']);
 
-angular.module('punktlich').config(function ($httpProvider, $locationProvider, RestangularProvider) {
+angular.module('punktlich').config(function ($httpProvider, $locationProvider, RestangularProvider, cfpLoadingBarProvider) {
     $locationProvider.html5Mode(true);
     $locationProvider.hashPrefix('!');
     RestangularProvider.setBaseUrl('api/v1/');
+
+    cfpLoadingBarProvider.includeSpinner = false;
 });
 
 angular.module('punktlichDep', [
