707df448166e79a6eed81192ec5f7672ce13b3e4
Daan Houweling <daan.gdh@gmail.com>
2014-12-09 22:46:25 +0100

    Code cleanup

--- base/static/app/services/meetingService.js
+++ base/static/app/services/meetingService.js
@@ -1,39 +1,4 @@
-angular.module('punktlichDep').service('MeetingService', function (MeetingModel, Restangular) {
-
-    Restangular.extendModel('meetings', function (meeting) {
-
-        //@todo maybe we should create somehing generic to parse dates
-        meeting.date_and_time_moment = moment(new Date(meeting.date_and_time));
-
-        meeting.future = function () {
-            return meeting.date_and_time_moment.isAfter(moment());
-        };
-
-        meeting.humanReadableDate = function () {
-            return meeting.future() ? 'Starts' : 'Started' + ' ' + meeting.date_and_time_moment.fromNow();
-        };
-
-        var postFile = function (type, file, success, error) {
-            var formData = new FormData();
-            formData.append('file', file);
-            meeting.withHttpConfig({transformRequest: angular.identity})
-                .customPOST(formData, 'agenda', undefined, {'Content-Type': undefined}).then(success, error);
-        };
-
-        meeting.postAgenda = function (file, success, error) {
-            postFile('agenda', file, success, error);
-        };
-
-        meeting.postMinute = function (file, success, error) {
-            postFile('minute', file, success, error);
-        };
-
-        meeting.postAttachment = function (file, success, error) {
-            postFile('attachment', file, success, error);
-        };
-
-        return meeting;
-    });
+angular.module('punktlichDep').service('MeetingService', function (MeetingModel) {
 
     function update(meeting, success, error) {
         meeting.put().then(success, error);
