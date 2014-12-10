458624d3fec205104096d04646a699697d5ccfe0
Leon Huzen <leonhuzen@gmail.com>
2014-12-10 15:31:08 +0100

    Remove old

--- base/static/app/controllers/teamCrudController.js
+++ base/static/app/controllers/teamCrudController.js
@@ -1,7 +1,6 @@
 angular.module('punktlichDep').controller('TeamUpdateController', function ($scope, TeamService, Restangular, $stateParams, ValidationService) {
     TeamService.get($stateParams.id).get().then(function (data) {
         $scope.team = data;
-        console.log($scope.team.getMembers());
     });
 
     $scope.save = function (form) {
--- base/static/app/services/teamService.js
+++ base/static/app/services/teamService.js
@@ -5,9 +5,6 @@ angular.module('punktlichDep').service('TeamService', function (TeamModel, UserM
         //@todo maybe we should create somehing generic to parse dates
         team.created_on_moment = moment(new Date(team.created_on));
 
-        team.getMembers = function () {
-        };
-
         return team;
     });
 
