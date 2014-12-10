fa0bdf6c2ba6a558dfa43226b05d72232ea8f7cf
Luuk Holleman <luukholleman@gmail.com>
2014-12-10 14:58:14 +0100

    Added color api call to return which color a group has

--- api/team/views.py
+++ api/team/views.py
@@ -2,6 +2,7 @@ from django.http import Http404
 from rest_framework import viewsets
 from rest_framework.decorators import detail_route
 from rest_framework.response import Response
+from api.authentication.serializers import ColorSerializer
 from api.team.serializers import TeamSerializer
 from team.models import Team
 
@@ -20,3 +21,8 @@ class TeamViewSet(viewsets.ModelViewSet):
     def list(self, request, *args, **kwargs):
         return super(TeamViewSet, self).list(request)
 
+    @detail_route(methods=['GET'])
+    def color(self, request, pk=None):
+        color = self.get_object().user_color.filter(user_id=request.user.id).get().color
+
+        return Response(ColorSerializer(color).data)
