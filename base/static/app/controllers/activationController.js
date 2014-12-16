951a78c75a8ec56f4ac423543ccc094581e30ffe
Tim Potze <timpotze@gmail.com>
2014-12-16 12:24:35 +0100

    Fixed activation controller

--- base/static/app/controllers/activationController.js
+++ base/static/app/controllers/activationController.js
@@ -1,16 +1,16 @@
-angular.module('punktlichDep').controller('ActivationController', function ($scope, $location, $routeParams, $http) {
+angular.module('punktlichDep').controller('ActivationController', function ($scope, $state, $routeParams, $http, FlashMessageService) {
     var token = $routeParams.token;
 
     if (token == null) {
-        $location.path('/login');
+        $state.go('login');
         return;
     }
 
-    $http.post('/api/v1/users/activate/', {activation_token: token}).success(function(data, status, headers) {
-        $location.path('/login');
-        $scope.flash.setMessage('Your account was activated successfully! You can now login!');
-    }).error(function(data, status, headres) {
-        $scope.flash.setMessage('Your account could not be activated');
+    $http.post('/api/v1/users/activate/', {activation_token: token}).success(function() {
+        $state.go('login');
+        FlashMessageService.setMessage('Your account was activated successfully! You can now login!');
+    }).error(function() {
+        FlashMessageService.setMessage('Your account could not be activated');
     });
 
 });
