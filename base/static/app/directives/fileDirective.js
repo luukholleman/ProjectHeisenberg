42ca8b022c802a561121856840d6813f1ecb9f4c
Tim Potze <timpotze@gmail.com>
2014-12-17 09:58:10 +0100

    Fixed file uploading in safari

--- base/static/app/directives/fileDirective.js
+++ base/static/app/directives/fileDirective.js
@@ -1,15 +1,22 @@
-angular.module('punktlichDep').directive('fileModel', function ($parse) {
+angular.module('punktlichDep').directive('ngFile', function ($parse, $rootScope) {
     return {
         restrict: 'A',
-        link: function (scope, element, attrs) {
-            var model = $parse(attrs.fileModel);
-            var modelSetter = model.assign;
-
-            element.bind('change', function () {
-                scope.$apply(function () {
-                    modelSetter(scope, element[0].files[0]);
-                });
-            });
+        compile: function ($element, attrs) {
+            return function ngEventHandler(scope, element) {
+                var fn = $parse(attrs['ngFile'], null, true);
+                element[0].onchange = function (event) {
+                    if (event.files = element[0].files) {
+                        var callback = function () {
+                            fn(scope, {$event: event});
+                        };
+                        if ($rootScope.$$phase) {
+                            scope.$evalAsync(callback);
+                        } else {
+                            scope.$apply(callback);
+                        }
+                    }
+                };
+            };
         }
     };
 });
