026f79a1b4eabfa92d47fe0c2080f658d83958db
Daan Houweling <daan.gdh@gmail.com>
2014-12-17 15:31:00 +0100

    Added Api endpoint to add user to team by email

--- api/team/urls.py
+++ api/team/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from rest_framework import routers
-from api.team.views import TeamViewSet, TeamMemberApiView, TeamMemberListApiView
+from api.team.views import TeamViewSet, TeamMemberApiView, TeamMemberListApiView, InviteTeamMemberApiView
 
 router = routers.DefaultRouter(trailing_slash=False)
 
@@ -8,6 +8,7 @@ router.register(r'teams', TeamViewSet, base_name='api.v1.team')
 
 urlpatterns = [
     url(r'^teams/(?P<teamId>\d+)/members$', TeamMemberListApiView.as_view(), name='team-members'),
-    url(r'^teams/(?P<teamId>\d+)/members/(?P<pk>\d+)$', TeamMemberApiView.as_view(), name='team-member'),
+    url(r'^teams/(?P<teamId>\d+)/members/invite$', InviteTeamMemberApiView.as_view(), name='team-members'),
+    url(r'^teams/(?P<teamId>\d+)/members/(?P<pk>\d+)$', TeamMemberApiView.as_view(), name='invite-team'),
     url(r'^', include(router.urls))
 ]
