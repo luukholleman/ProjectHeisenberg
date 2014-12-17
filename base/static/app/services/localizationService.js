55ec8e51088340ca6033d3501c4678da4b26ca35
Tim Potze <timpotze@gmail.com>
2014-12-17 12:07:57 +0100

    Fixed some bugs related to previous commit

--- base/static/app/services/localizationService.js
+++ base/static/app/services/localizationService.js
@@ -1,4 +1,4 @@
-angular.module('punktlichDep').service('LocalizationService', function ($rootScope, $timeout, $window, $http) {
+angular.module('punktlichDep').service('LocalizationService', function ($q, $rootScope, $timeout, $window, $http) {
     var DEFAULT_LOCALE = 'en-us';
     var LOCALE_URL = '/static/app/locale/{0}.json';
     var ALLOW_CACHE = false;
@@ -15,27 +15,37 @@ angular.module('punktlichDep').service('LocalizationService', function ($rootSco
     }
 
     function localize(name) {
-        if (!isLoading && !isLoaded) {
-            loadLocale(defaultLanguage);//for now, load default locale
-        }
-
-        return isLoaded ? (dictionary[name] || name) : null;
+        return isLoaded ? (dictionary[name] || name) : name;
     }
 
     function load() {
         return loadLocale(defaultLanguage);//for now, load default locale
     }
 
-    function onSuccess(data) {
-        dictionary = data;
-        isLoaded = true;
-        isLoading = false;
-
-        $rootScope.$emit('localizationResourcesUpdated');
-    };
-
     function loadLocale(key) {
         isLoading = true;
+
+        return $q(function(resolve) {
+            function onSuccess(data) {
+                dictionary = data;
+                isLoaded = true;
+                isLoading = false;
+
+                resolve();
+            };
+
+            $http({method: "GET", url: LOCALE_URL.replace('{0}', key), cache: ALLOW_CACHE})
+                .success(onSuccess)
+                .error(function () {
+                    console.error('locale', key, 'could not be loaded, falling back to', DEFAULT_LOCALE);
+                    $http({method: "GET", url: LOCALE_URL.replace('{0}', DEFAULT_LOCALE), cache: ALLOW_CACHE})
+                        .success(onSuccess)
+                        .error(function () {
+                            console.error('Failed to load default localization. Falling back to keys');
+                            onSuccess({});
+                        });
+                });
+        });
 
         return $http({method: "GET", url: LOCALE_URL.replace('{0}', key), cache: ALLOW_CACHE})
             .success(onSuccess)
