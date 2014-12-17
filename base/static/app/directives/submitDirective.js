2d269d12c9e9ff1d1cb32c4369f16cf62ae8ebca
Tim Potze <timpotze@gmail.com>
2014-12-17 15:20:53 +0100

    Ignore after return

--- base/static/app/directives/submitDirective.js
+++ base/static/app/directives/submitDirective.js
@@ -7,6 +7,8 @@ angular.module('punktlichDep').directive('plSubmit', function ($parse, $rootScop
                 _.forEach(element[0].querySelectorAll('input'), function (input) {
                     input.addEventListener('keypress', function (e) {
                         if (e.which == 10 || e.which == 13) {
+                            e.preventDefault();
+
                             var callback = function () {
                                 fn(scope, {$event: event});
                             };
